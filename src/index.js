2e70f2b98d9835e4594aa9a8e60b65aea18eb4cc
Maximilian Stoiber <contact@mxstbr.com>
2018-09-04 10:39:55 +0200

    Update deps

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-// @flow
-export const sum = (a: number, b: number) => {
+// @flow strict
+
+export const sum = (a: number, b: number): Promise<number> => {
   return new Promise(res => res(a + b));
 };
